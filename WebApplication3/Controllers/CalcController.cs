4e169dfd390104f81859cc3df765b2953a1c87be
HalimHamidov <halimjonik@gmail.com>
2020-10-22 21:23:58 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3/Controllers/CalcController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index(CalcM c)
+        {
+            switch(c.Op)
+            {
+                case '+': c.Result = c.Number1 + c.Number2;
+                    break;
+                case '-':
+                    c.Result = c.Number1 - c.Number2;
+                    break;
+                case '*':
+                    c.Result = c.Number1 * c.Number2;
+                    break;
+                case '/':
+                    c.Result = c.Number1 / c.Number2;
+                    break;
+            }
+            return View(c);
+        }
+
+
+    }
+}
---
+++ WebApplication3/Models/CalcM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class CalcM
+    {
+        public int Number1 {get; set;}
+        public int Number2 {get; set;}
+        public char Op {get; set;}
+        public int Result { get; set; }
+    }
+}
